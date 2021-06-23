cd821a5c32165c94be2d592154c8df843a218532
mahdi salamat <fannimh36@gmail.com>
2021-06-23 02:40:51 -0700

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import { div } from 'prelude-ls';
 import React from 'react';
 import {Switch ,Route} from 'react-router-dom'
-import ShopPage from './pages/shope/shop.component';
 
 import './App.css';
 
+import ShopPage from './pages/shope/shop.component';
 import HomePage from './pages/homepage/homepage.component';
+import SingInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import {auth} from './firebase/firebase.utils';
 
 
-function App() {
-  return ( 
-  <div> 
-    <Header/>
-    <switch>
-    <Route exact path='/' component={HomePage}/>
-    <Route  path='/shop' component={ShopPage}/>
-    </switch>
-    
-  </div>
-  )
+
+class App extends React.Component  {
+  constructor (){
+    super();
+
+    this.state = {
+      currentUser : null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+componentDidMount(){
+  this.unsubscribeFromAuth = auth.onAuthStateChanged( user =>{
+    this.setState({ currentUser:user });
+  })
+}
+
+componentWillUnmount(){
+  this.unsubscribeFromAuth();
+}
+
+  render(){
+    return ( 
+      <div> 
+        <Header currentUser={this.state.currentUser}/>
+        <switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route  path='/shop' component={ShopPage}/>
+        <Route  path='/signin' component={SingInAndSignUpPage}/>
+        </switch>
+        
+      </div>
+      );
+  }
+  
   
 }
 
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyC-KPW4XZgh4qteUFi0MaHHL9-6X2M26Z8",
+    authDomain: "crwn-db-803aa.firebaseapp.com",
+    projectId: "crwn-db-803aa",
+    storageBucket: "crwn-db-803aa.appspot.com",
+    messagingSenderId: "161434677991",
+    appId: "1:161434677991:web:ff81304f678efddb49e842",
+    measurementId: "G-7MW965WK0Y"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt:'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
